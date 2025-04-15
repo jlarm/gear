eb8158ae828f3654425ed4986049cd6c659c4ae6
jlarm <117611704+jlarm@users.noreply.github.com>
2025-04-15 16:55:12 -0500

    Added price to item migration

--- database/migrations/2025_04_15_214249_create_items_table.php
+++ database/migrations/2025_04_15_214249_create_items_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('size')->nullable();
             $table->float('weight')->nullable();
             $table->string('color')->nullable();
+            $table->unsignedInteger('price')->nullable();
             $table->string('status');
             $table->dateTime('sold_at')->nullable();
             $table->timestamps();
